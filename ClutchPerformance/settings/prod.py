e63fd6b0039e3e64c45b030403152e9f0a0a8927
Spencer Ogden <spencer@spencerogden.com>
2017-01-07 14:49:32 -0500

    Add email settings for PROD

--- ClutchPerformance/settings/prod.py
+++ ClutchPerformance/settings/prod.py
@@ -1,3 +1,9 @@
 from .base import *
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+EMAIL_HOST = 'outbound.mailhop.org'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'spencerogden'
+EMAIL_HOST_PASSWORD = 'vrpt@7GPcwej'
+EMAIL_USE_SSL = True
