60b2d8658e71c5506a90c0dbfacccaf98654eee9
Spencer Ogden <spencer@spencerogden.com>
2017-01-07 15:05:11 -0500

    Fix reference, needed one level higher

--- ClutchPerformance/settings/base.py
+++ ClutchPerformance/settings/base.py
@@ -14,7 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+PROJECT_HOME = os.path.dirname(BASE_DIR)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
@@ -60,7 +60,7 @@ ROOT_URLCONF = 'ClutchPerformance.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [os.path.join(PROJECT_HOME, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
