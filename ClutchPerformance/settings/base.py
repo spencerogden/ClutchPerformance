80d2bba3f71d9bd81d58b5f05cbe698630406d6f
Spencer Ogden <spencer@spencerogden.com>
2017-03-18 16:19:00 -0400

    Add site name

--- ClutchPerformance/settings/base.py
+++ ClutchPerformance/settings/base.py
@@ -16,6 +16,8 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 PROJECT_HOME = os.path.dirname(BASE_DIR)
 
+SITE_NAME = "Clutch"
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
